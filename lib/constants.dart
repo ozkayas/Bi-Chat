029a343165789b5e344412a698e76c5b262f9b56
Ozkayas <suatozkaya@gmail.com>
2021-06-18 18:58:39 +0300

    Add login page

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,11 @@
+import 'dart:ui';
+
 class Constants {
   static final String appTitle = 'Bi-Chat';
+  //Secret string for two users, will be different for all customers
+  static final String passwordOne = 'abc';
+  static final String passwordTwo = 'xyz';
+
+  ///Colors:
+  static final primaryColor = Color(0xff203152);
 }
--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+import 'chat_page.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key key}) : super(key: key);
 
@@ -8,8 +11,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _name;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Spacer(
+              flex: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 80, 80, 20),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    width: 200,
+                    height: 200,
+                    child: Image.asset(
+                      'assets/logo.png',
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: 'Kullanıcı Adı'),
+                    onChanged: (value) {
+                      _name = value;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: 'Şifre'),
+                    onChanged: (value) {
+                      _password = value;
+                    },
+                    obscureText: true,
+                  ),
+                  Container(
+                    constraints: BoxConstraints(minWidth: 200),
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: ElevatedButton(
+                        child: Text('GİRİŞ'),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.redAccent)),
+                  )
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            TextButton(
+              child: Text(
+                '@2021',
+                style: TextStyle(color: Colors.pink.shade200, fontSize: 10),
+              ),
+              onPressed: () {
+                if (_password == Constants.passwordOne ||
+                    _password == Constants.passwordTwo) {
+                  //Clear Password before navigation
+                  var passArgument = _password;
+                  _password = '';
+                  //Navigate to ChatPage
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatPage(
+                        userId: passArgument,
+                      ),
+                    ),
+                  );
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
