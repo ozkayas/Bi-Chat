eaa384c05ec0db7932c64c6f7d139be573380a73
Ozkayas <suatozkaya@gmail.com>
2021-06-18 22:06:36 +0300

    Add model class for message & json_serial package

---
+++ lib/model/message.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+part 'message.g.dart';
+
+@JsonSerializable()
+class Message {
+  final String content;
+  final String idFrom;
+  final String idTo;
+  final DateTime timestamp;
+
+  Message(
+      {@required this.content,
+      @required this.idFrom,
+      @required this.idTo,
+      @required this.timestamp});
+
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
+  Map<String, dynamic> toJson() => _$MessageToJson(this);
+}
